cdcfbd3006e11ec0040a5c5db59dc76882ec5ccd
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-08-26 20:25:37 -0400

    homepage.js

---
+++ my-app/src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>
+                Alejandro Cortez Home Page
+            </p>
+        </div>
+
+    );
+}
