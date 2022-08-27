49f8e1f6d5514ac177b870ed67f9924a26cdf282
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-08-26 20:17:28 -0400

    updated about.js

---
+++ my-app/src/components/pages/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        I am a descendent of Mexican parents, a born and raised native of NYC.
+      </p>
+
+      <p>
+        Currently the Director of Dining Operations for the Guggenheim Museum. I
+        am a graduate of the French Culinary Institute and have been working in
+        the Restaurant / Hospitality industry for over 19 years.
+      </p>
+
+      <p>
+        As someone who's always loved technology and wondered the intricacies
+        behind them, I decided to take the leap and start a new journey and
+        career path.
+      </p>
+
+      <p>
+        I have enrolled in Columbia University's Engineering Coding Boot Camp to
+        transition as a Full-Stack Software Engineer. The knowledge and
+        experience I am developing in this bootcamp will provide the
+        fundamentals to the vast array of technologies, languages, frameworks,
+        and the processes behind them.
+      </p>
+
+      <p>
+        I am excited to start this incredible journey to grow in the tech
+        industry and acheive my end goal in a blossoming career in Cloud
+        Computing.
+      </p>
+    </div>
+  );
+}
---
+++ my-app/src/components/pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Resume() {
+  return(
+    <div>
+        <h1>My Resume</h1>
+        <p>
+            
+        </p>
+    </div>
+  );
+}
